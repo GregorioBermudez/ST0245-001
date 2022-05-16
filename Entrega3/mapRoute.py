60cf398ce46a4715d1e258cf64bedd784fc307e0
Samuel Rico Gómez <100243610+sricog@users.noreply.github.com>
2022-05-16 10:34:55 -0500

    Update mapRoute.py

--- Entrega3/mapRoute.py
+++ Entrega3/mapRoute.py
@@ -1,5 +1,16 @@
 import gmplot
-
+import pandas as pd
+apikey = 'AIzaSyBFSjXTLiGgLYvA_-SrCaoUWfBAouEgEUo'
+def crearLimitesPoligono():
+    lngs = []
+    lats = []
+    area = pd.read_csv('poligono_de_medellin.csv',sep=';')
+    poligon = str(area['geometry'].to_list()[0])[9:-2].split(',')
+    for coordenada in poligon:
+        long,lat = list(map(float,coordenada[1:].split(' ')))
+        lngs.append(long)
+        lats.append(lat)
+    return lats,lngs 
 def coordenadas(path):
     lat = []
     long = []
@@ -9,10 +20,22 @@ def coordenadas(path):
         lat.append(latt)
     return lat,long
 
-def graficarMapa(path):
-    lat,long = coordenadas(path)
-    gmapone = gmplot.GoogleMapPlotter(6.199328, -75.579521,14)
+def graficarMapa(path1,path2,path3):
+    latLimit, longLimit = crearLimitesPoligono()
+    lat,long = coordenadas(path1)
+    lat2,long2 = coordenadas(path2)
+    lat3,long3 = coordenadas(path3)
+    gmapone = gmplot.GoogleMapPlotter(
+    6.267203842477565, -75.579710387, 12,
+    apikey=apikey,)
+    gmapone.polygon(latLimit,longLimit,face_color='white',
+                    face_alpha = 0.4, edge_color='black', edge_width=10)
+    gmapone.scatter(lat,long,'blue',size = 3,marker = False)
+    gmapone.plot(lat,long,'blue',edge_width = 4)
+    gmapone.scatter(lat,long,'green',size = 3,marker = False)
+    gmapone.plot(lat2,long2,'green',edge_width = 5)
     gmapone.scatter(lat,long,'red',size = 3,marker = False)
-    gmapone.plot(lat,long,'green',edge_width = 5)
+    gmapone.plot(lat3,long3,'red',edge_width = 6)
+    gmapone.marker(lat[0],long[0],label = 'A')
+    gmapone.marker(lat[-1],long[-1],label = 'B')
     gmapone.draw('map.html')
-    
