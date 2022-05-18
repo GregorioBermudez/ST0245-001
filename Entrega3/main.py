3ed9f2c9719d870bbe155efc3994ed9175c3a9d0
Samuel Rico Gómez <100243610+sricog@users.noreply.github.com>
2022-05-18 15:45:02 -0500

    Update main.py

--- Entrega3/main.py
+++ Entrega3/main.py
@@ -3,52 +3,45 @@ import JsonApi
 import mapRoute
 import graphAlgorithms
 import webbrowser
-
 data = pd.read_csv('calles_de_medellin_con_acoso.csv', sep = ';',)
 data.harassmentRisk = data.harassmentRisk.fillna(data.harassmentRisk.mean())
-grafo= {}
-origenes_unicos = data.origin.unique()
-# En este ciclo, se crea grafo como diccionario de diccionarios. 
-# Inicialmente, las llaves son los origenes
-# que estan en el dataframe 'data'
-# Y los valores son diccionarios vacios.
-for i in range(len(origenes_unicos)):
-    grafo[origenes_unicos[i]] = {}
-# Se completa el grafo. En el diccionario de cada valor,
-# las llaves son los destinos, y los valores son
-# la distancia y el porcentaje de acoso para ese destino
-# Se tiene en cuenta el valor de 'oneway' en el dataframe, 
-# ya que si es Verdadero, la relacion origen-destino es simetrica, y se debe
-# añadir en ambos sentidos al diccionario.
+graph= {}
+unique_origins = data.origin.unique()
+
+# The graph is represented as a dict of dicts
+# Keys are the origins and at first their values are empty dicts.
+for i in range(len(unique_origins)):
+    graph[unique_origins[i]] = {}
+
+# The values are filled.
+# The keys are the destinations and the values are the distance, and
+# harassment risk for each destination.
 for i in data.index:
     if data["oneway"][i]==False:
-        grafo[data["origin"][i]][data["destination"][i]]=(data["length"][i],data["harassmentRisk"][i])
+        graph[data["origin"][i]][data["destination"][i]]=(data["length"][i],data["harassmentRisk"][i])
     else:
-        grafo[data["origin"][i]][data["destination"][i]]=(data["length"][i],data["harassmentRisk"][i])
+        graph[data["origin"][i]][data["destination"][i]]=(data["length"][i],data["harassmentRisk"][i])
         try:
-            grafo[data["destination"][i]][data["origin"][i]]=(data["length"][i],data["harassmentRisk"][i])
-        except KeyError: # Este error se da cuando, el valor de 'oneway' es verdadero, y el destino debe ser un origen en el diccionario.
-                         # En ciertos casos, ese destino no se encontraba como origen en el dataframe, por lo que no se encontraba en 
-                         # 'grafos' y tratar de acceder a este genera 'KeyError'. La solucion es simplemente
-                         # añadirlo como una nueva llave y crear el otro diccionario en el valor.
-            grafo[data['destination'][i]]={data["origin"][i]:(data["length"][i],data["harassmentRisk"][i])}
+            graph[data["destination"][i]][data["origin"][i]]=(data["length"][i],data["harassmentRisk"][i])
+        except KeyError:#  This error happens when 'oneway' is True and the destination must be as a key in the dict as a origin.
+                        # In some cases, this coordinates where not presented as a origin in the dataframe.
+            graph[data['destination'][i]]={data["origin"][i]:(data["length"][i],data["harassmentRisk"][i])}
 
 
 def main():
     print('Por favor, sea muy específico al ingresar los lugares...')
     print("Recuerde que la ruta roja es la más rápida, la azul la más balanceada entre acoso y distancia, y la verde es la más segura.")
-    origen = JsonApi.generar_coordenadas(input("Ingrese lugar de origen: "))
-    origenGrafo = graphAlgorithms.encontrarOrigenCercano(origen,data)
-    origenGrafo = str((origenGrafo[1],origenGrafo[0]))
-    destino =JsonApi.generar_coordenadas(input("Ingrese lugar de destino: "))
-    destinoGrafo = graphAlgorithms.encontrarDestinoCercano(destino,data)
-    destinoGrafo = str((destinoGrafo[1],destinoGrafo[0]))
-    path1, totalDistance = graphAlgorithms.safest_path(origenGrafo,destinoGrafo,grafo)
-    path2, totalDistance2 = graphAlgorithms.shortest_and_safest_path(origenGrafo,destinoGrafo,grafo)
-    path3, totalDistance3 = graphAlgorithms.shortest_path(origenGrafo,destinoGrafo,grafo)
+    origin = graphAlgorithms.nearestOrigin(JsonApi.strToCoordinates(input("Ingrese lugar de origen: ")),data)
+    origin = str((origin[1],origin[0]))
+    destination = graphAlgorithms.nearestDestination(JsonApi.strToCoordinates(input("Ingrese lugar de destino: ")),data)
+    destination = str((destination[1],destination[0]))
+    path1, totalDistance = graphAlgorithms.safest_path(origin,destination,graph)
+    path2, totalDistance2 = graphAlgorithms.shortest_and_safest_path(origin,destination,graph)
+    path3, totalDistance3 = graphAlgorithms.shortest_path(origin,destination,graph)
     
-    mapRoute.graficarMapa(path1,path2,path3)
+    mapRoute.createMap(path1,path2,path3)
     webbrowser.open_new_tab('map.html')
 
 main()
+
 
