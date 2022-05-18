dda4843f8043c78020cc07d7e1a3c32f3e93e570
Samuel Rico Gómez <100243610+sricog@users.noreply.github.com>
2022-05-18 15:46:29 -0500

    Update mapRoute.py

--- Entrega3/mapRoute.py
+++ Entrega3/mapRoute.py
@@ -1,30 +1,30 @@
 import gmplot
 import pandas as pd
-apikey = '' #insertar API de google maps
-def crearLimitesPoligono():
+apikey = 'AIzaSyBFSjXTLiGgLYvA_-SrCaoUWfBAouEgEUo'
+def createPolygon():
     lngs = []
     lats = []
     area = pd.read_csv('poligono_de_medellin.csv',sep=';')
-    poligon = str(area['geometry'].to_list()[0])[9:-2].split(',')
-    for coordenada in poligon:
-        long,lat = list(map(float,coordenada[1:].split(' ')))
+    polygon = str(area['geometry'].to_list()[0])[9:-2].split(',')
+    for coord in polygon:
+        long,lat = list(map(float,coord[1:].split(' ')))
         lngs.append(long)
         lats.append(lat)
     return lats,lngs 
-def coordenadas(path):
+def organizeCoordinates(path):
     lat = []
     long = []
-    for coordenada in path:
-        longg,latt = list(map(float,coordenada[1:-1].split(',')))
+    for coord in path:
+        longg,latt = list(map(float,coord[1:-1].split(',')))
         long.append(longg)
         lat.append(latt)
     return lat,long
 
-def graficarMapa(path1,path2,path3):
-    latLimit, longLimit = crearLimitesPoligono()
-    lat,long = coordenadas(path1)
-    lat2,long2 = coordenadas(path2)
-    lat3,long3 = coordenadas(path3)
+def createMap(path1,path2,path3):
+    latLimit, longLimit = createPolygon()
+    lat,long = organizeCoordinates(path1)
+    lat2,long2 = organizeCoordinates(path2)
+    lat3,long3 = organizeCoordinates(path3)
     gmapone = gmplot.GoogleMapPlotter(
     6.267203842477565, -75.579710387, 12,
     apikey=apikey,)
@@ -36,6 +36,7 @@ def graficarMapa(path1,path2,path3):
     gmapone.plot(lat2,long2,'green',edge_width = 5)
     gmapone.scatter(lat,long,'red',size = 3,marker = False)
     gmapone.plot(lat3,long3,'red',edge_width = 6)
-    gmapone.marker(lat[0],long[0],label = 'A')
+    gmapone.marker(lat[0],long[0],label = 'A',title='Punto Inicio')
     gmapone.marker(lat[-1],long[-1],label = 'B')
     gmapone.draw('map.html')
+    
